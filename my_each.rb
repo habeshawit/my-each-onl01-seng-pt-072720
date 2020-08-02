76397eda56c956ff5f84e6631cbb803c89a9bcd7
Shalom Kebede <shalomseleshi@gmail.com>
2020-08-02 02:51:12 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  i=0
+  while i < collection.length
+    yield(collection)
+    i+=1
+  end
+  collection
+end
+
+my_each(collection) do |i|
+  puts i
 end
